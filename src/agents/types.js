5e80c1757ef0cbeb9a49ddfd719c8408a70f5f75
iamtalhaarifvlogs <iamtalhaarifvlogs@gmail.com>
2026-07-29 03:54:50 +0500

    Update types.js

--- src/agents/types.js
+++ src/agents/types.js
@@ -1,1 +1,27 @@
+// src/agents/types.js
+
+/**
+ * @typedef {Object} TaskPacket
+ * @property {string} taskId
+ * @property {'QUERY_DATABASE' | 'GENERATE_PDF' | 'ANALYZE_PIPELINE'} actionType
+ * @property {Object} parameters
+ * @property {Object} [authContext]
+ */
+
+/**
+ * @typedef {Object} TaskResponse
+ * @property {string} taskId
+ * @property {'SUCCESS' | 'ERROR'} status
+ * @property {Object} [dataPayload]
+ * @property {string} [filePath]
+ * @property {string} [errorMessage]
+ */
+
+/**
+ * @typedef {Object} MayaResponse
+ * @property {string} advice
+ * @property {TaskResponse} [executedTask]
+ */
+
+export {};
 
